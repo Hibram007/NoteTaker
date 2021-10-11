be0979ae5e270383b6bb387ddc81fb7a6291293f
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-10-10 20:54:45 -0700

    MVP of notetaker- server created linked to JSON

--- server.js
+++ server.js
@@ -6,28 +6,34 @@ const express = require('express');
 const { db } = require('./db/db.json');
 
 // adding the port for HEROKU - runs on 80 PORT
-const port = process.env.port || 3001
-
-
-
+const PORT = process.env.PORT || 3001
 
 //instantiate server
 const app = express();
 
+function filterByQuery(query, dbArray) {
+    let filteredResults = dbArray;
+    if (query.title) {
+        filteredResults = filteredResults.filter(db => db.title === query.title);
+    }
+    if (query.text) {
+        filteredResults = filteredResults.filter(db => db.text === query.text);
+      }
+      return filteredResults;
+    }
+
 //route for data from db.json - WORKING!!
+// query added to search for specif data in array
 app.get('/api/db', (req,res) => {
-    res.send('Working!');
+    let results = db;
+   if (req.query) {
+       results = filterByQuery(req.query, results);
+   }
+    res.json(results);
 });
-
-// app.get('/api/db', (req, res) => {
-//     let results = db;
-//     console.log(req.query)
-//     res.json(results);
-// });
-
 
 // listen method for requests to server
 //n.b. - heroku runs on 80 PORT so this might cause issues- adapt when needed
-app.listen(port, () => {
-    console.log('API server now on port {port}!');
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
