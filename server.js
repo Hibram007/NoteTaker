8e0548f2bee6f1edbc365e15cc338d668f8a8865
Hibram007 <87105978+Hibram007@users.noreply.github.com>
2021-11-30 13:51:58 -0700

    file structure altered, index.js modified, white space removed,  file paths to db adjusted, attempt 2 a solving Get route error

--- server.js
+++ server.js
@@ -1,39 +1,59 @@
-// require statments to use express.js
+const { notStrictEqual } = require('assert');
 const express = require('express');
+const fs = require("fs");
+const path = require('path')
+// Specifically for Heroku Deployment
+const port = process.env.port || 3001
 
-//request for db.json - route to request data
-// this is the test json file to connect back to front end
-const { db } = require('./db/db.json');
-
-// adding the port for HEROKU - runs on 80 PORT
-const PORT = process.env.PORT || 3001
-
-//instantiate server
+// initialize an express application
 const app = express();
 
-function filterByQuery(query, dbArray) {
-    let filteredResults = dbArray;
-    if (query.title) {
-        filteredResults = filteredResults.filter(db => db.title === query.title);
-    }
-    if (query.text) {
-        filteredResults = filteredResults.filter(db => db.text === query.text);
-      }
-      return filteredResults;
-    }
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//route for data from db.json - WORKING!!
-// query added to search for specif data in array
-app.get('/api/db', (req,res) => {
-    let results = db;
-   if (req.query) {
-       results = filterByQuery(req.query, results);
-   }
-    res.json(results);
-});
+// API ROUTES FOR CRUD METHODS
 
-// listen method for requests to server
-//n.b. - heroku runs on 80 PORT so this might cause issues- adapt when needed
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+// READ METHOD route - get
+app.get('/api/notes', (req, res) => {
+    fs.readFile('db\db.json', 'utf8', (err, data) => {
+        res.json(JSON.parse(data))
+    })
+})
+
+// CREATE Method route- POST
+app.post('/api/notes', (req, res) => {
+    fs.readFile('db\db.json', 'utf8', (err, data) => {
+        const dbData = JSON.parse(data);
+        req.body.id = dbData.length + 1;
+        dbData.push(req.body);
+        fs.writeFile('db\db.json', JSON.stringify(dbData), (err) => {
+            res.send('Note has been added!')
+        })
+    })
+})
+// DELETE Methods - Destroy
+app.delete("/api/notes/:id", (req, res) => {
+             fs.readFile('db\db.json', 'utf8', (err, data) => {
+                 const dbData = JSON.parse(data); 
+                const filteredNotes = dbData.filter(function(note) {
+                    return note.id != req.params.id;
+           })
+             fs.writeFile('db\db.json', JSON.stringify(filteredNotes), (err) => {
+                      res.send('Note has been added!')
+                 })
+            })
 });
+// html  routes - For pre-made module 11 code
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public\notes.html'));
+    })
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'public\index.html'));
+    })
+
+// Link to connect DB and Server ( Starts it all)
+    app.listen(process.env.PORT || 3001, () => {
+        console.log('App is now listening to PORT 3001!')
+    })
